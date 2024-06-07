d4337232a115edfc20fea05170f56d1debee82c9
Yosef1997 <yosefsitumorang97@gmail.com>
2024-06-07 13:52:53 +0700

    first commit

---
+++ src/main/java/com/yosef/moontrack/MoontrackApplication.java
@@ -0,0 +1,13 @@
+package com.yosef.moontrack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoontrackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoontrackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/yosef/moontrack/MoontrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yosef.moontrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoontrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
